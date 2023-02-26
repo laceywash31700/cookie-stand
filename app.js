a58672ebe031f4ee96b6bbc45e350090285b27ea
lacey washington <lacey.m.washington@gmail.com>
2023-02-25 22:08:58 -0600

    did some re-organizing

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const hours = ['Hours','6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm','Total'];
+const hours = ['Hours', '6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', 'Total'];
 let allTheCities = [];
 const sectionElem = document.getElementById('holds table');
 const tableElem = document.createElement('table');
@@ -23,34 +23,8 @@ City.prototype.totalCookiesGen = function () {
   for (let i = 0; i < this.numberOfCookiesPerHr.length; i++) {
     this.totalCookiesSold += this.numberOfCookiesPerHr[i];
   }
-  this.numberOfCookiesPerHr.push(`${this.totalCookiesSold} sold today`);
+  this.numberOfCookiesPerHr.push(`${this.totalCookiesSold} sold`);
 };
-
-const seattle = new City('Seattle', 23, 65, 6.3);
-const tokyo = new City('Tokyo', 3, 24, 1.2);
-const dubai = new City('Dubai', 11, 38, 3.7);
-const paris = new City('Paris', 20, 38, 2.3);
-const chicago = new City('Chicago', 2, 16, 4.6);
-
-seattle.genRandCustAndCookies();
-tokyo.genRandCustAndCookies();
-dubai.genRandCustAndCookies();
-paris.genRandCustAndCookies();
-chicago.genRandCustAndCookies();
-
-seattle.totalCookiesGen();
-tokyo.totalCookiesGen();
-dubai.totalCookiesGen();
-paris.totalCookiesGen();
-chicago.totalCookiesGen();
-
-console.log(seattle);
-console.log(tokyo);
-console.log(dubai);
-console.log(paris);
-console.log(chicago);
-console.log(allTheCities);
-
 function renderTable() {
   const tableRowElem = document.createElement('tr');
   tableElem.appendChild(tableRowElem);
@@ -75,7 +49,7 @@ function renderCityTableRow() {
     tableHeadElem.textContent = allTheCities[i].city;
     tableRowElem.appendChild(tableHeadElem);
     console.log(allTheCities[i]);
-    for (let j = 0; j < allTheCities[i].numberOfCookiesPerHr.length; j++){
+    for (let j = 0; j < allTheCities[i].numberOfCookiesPerHr.length; j++) {
       const tableDataElem = document.createElement('td');
       tableDataElem.textContent = allTheCities[i].numberOfCookiesPerHr[j];
       tableRowElem.appendChild(tableDataElem);
@@ -84,6 +58,30 @@ function renderCityTableRow() {
   }
 }
 
+const seattle = new City('Seattle', 23, 65, 6.3);
+const tokyo = new City('Tokyo', 3, 24, 1.2);
+const dubai = new City('Dubai', 11, 38, 3.7);
+const paris = new City('Paris', 20, 38, 2.3);
+const chicago = new City('Chicago', 2, 16, 4.6);
+
+seattle.genRandCustAndCookies();
+tokyo.genRandCustAndCookies();
+dubai.genRandCustAndCookies();
+paris.genRandCustAndCookies();
+chicago.genRandCustAndCookies();
+
+seattle.totalCookiesGen();
+tokyo.totalCookiesGen();
+dubai.totalCookiesGen();
+paris.totalCookiesGen();
+chicago.totalCookiesGen();
 
 renderTable();
 renderCityTableRow();
+
+console.log(seattle);
+console.log(tokyo);
+console.log(dubai);
+console.log(paris);
+console.log(chicago);
+console.log(allTheCities);
